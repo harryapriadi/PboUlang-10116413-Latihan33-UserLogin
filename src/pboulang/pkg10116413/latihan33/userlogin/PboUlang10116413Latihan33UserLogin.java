f5d7603066aabf003dbe5839b6823c7d04c19a4d
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:01:46 +0700

    PboUlang-10116413-Latihan33-UserLogin

---
+++ src/pboulang/pkg10116413/latihan33/userlogin/PboUlang10116413Latihan33UserLogin.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan33.userlogin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : Program ini berisi program yang untuk user login
+ */
+public class PboUlang10116413Latihan33UserLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static String usName;
+    private static String passWord;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Masukkan Username = ");
+        usName = scn.next();
+
+        System.out.print("Masukkan Password = ");
+        passWord = scn.next();
+
+        User cek = new User();
+        cek.pengecekkanLogin(usName,passWord);
+
+    }
+    
+}
